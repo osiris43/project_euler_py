533d79b3c0f1df36fe4cdffff4818e0ffc3375c6
Brett Bim <osiris43@gmail.com>
2014-03-11 06:52:37 -0500

    improving performance for determining primes

--- number5.py
+++ number5.py
@@ -1,15 +1,12 @@
-import math
+import lib
 
 def prime_factorization(n):
-    def is_prime(n):
-        return not [x for x in xrange(2, int(math.sqrt(n))) if n%x==0]
-
     primes = []
     candidates = xrange(2, n+1)
     candidate = 2
 
     while not primes and candidate in candidates:
-        if n%candidate == 0 and is_prime(candidate):
+        if n%candidate == 0 and lib.is_prime(candidate):
             primes = primes + [candidate] + prime_factorization(n/candidate)
         candidate += 1
 
--- number7.py
+++ number7.py
@@ -1,15 +1,17 @@
-from lib import is_prime
+from lib import is_prime1
+import datetime
 
 def solve(n):
     counter = 0
     startingPrime = 1
     while counter < n:
         startingPrime += 1
-        if is_prime(startingPrime):
+        if is_prime1(startingPrime):
             counter += 1
 
     print startingPrime
 
 if __name__ == '__main__':
+    a = datetime.datetime.now()
     solve(10001)
-
+    print datetime.datetime.now() - a
